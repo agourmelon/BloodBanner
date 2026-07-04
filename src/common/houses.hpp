7c592a852aa5d8e6fa90cb7f848ca86b93fb96bf
Alexandre Gourmelon <alexandregourmelon@airdealexandre.home>
2026-07-04 17:45:45 +0200

    fix linter warnings in houses.hpp

--- src/common/houses.hpp
+++ src/common/houses.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "common/constants.hpp"
 #include <algorithm>
 #include <array>
 #include <format>
@@ -16,38 +17,45 @@ struct HouseCard {
     int              swords         = 0;
     int              towers         = 0;
 
-    bool operator==(const HouseCard&) const = default;
+    bool operator==(const HouseCard &) const = default;
 };
 
 class HouseCardHand {
     std::vector<HouseCard> availables_;
     std::vector<HouseCard> used_;
 
-  public:
-    HouseCardHand(std::array<HouseCard, 7>&& cards)
-        : availables_{std::make_move_iterator(std::begin(cards)),
-                      std::make_move_iterator(std::end(cards))},
-          used_{} {}
+public:
 
-    ~HouseCardHand()                = default;
-    HouseCardHand(HouseCardHand&)   = delete;
-    void operator=(HouseCardHand&)  = delete;
-    HouseCardHand(HouseCardHand&&)  = default;
-    void operator=(HouseCardHand&&) = delete;
+    explicit HouseCardHand(
+        std::array<HouseCard, HOUSE_CARDS_PER_PLAYER> &&
+            cards // NOLINT(cppcoreguidelines-rvalue-reference-param-not-moved)
+    )
+        : availables_{
+              std::make_move_iterator(std::begin(cards)),
+              std::make_move_iterator(std::end(cards))
+          } {}
 
-    const std::vector<HouseCard>& available() const { return availables_; }
-    const std::vector<HouseCard>& used() const { return used_; }
-    void                          discard(const HouseCard& card) {
+    ~HouseCardHand()                 = default;
+    HouseCardHand(HouseCardHand &)   = delete;
+    void operator=(HouseCardHand &)  = delete;
+    HouseCardHand(HouseCardHand &&)  = default;
+    void operator=(HouseCardHand &&) = delete;
+
+    [[nodiscard]] const std::vector<HouseCard> & available() const { return availables_; }
+    [[nodiscard]] const std::vector<HouseCard> & used() const { return used_; }
+    void                                         discard(const HouseCard & card) {
         auto it = std::ranges::find(availables_, card);
         if (it == availables_.end())
             throw std::out_of_range(std::format("{} is not found in hand", card.name));
         availables_.erase(it);
         if (availables_.size() > 0) {
             used_.push_back(card);
-        } else if (availables_.size() == 0) {
+        }
+        else if (availables_.size() == 0) {
             availables_ = std::move(used_);
             used_.push_back(card);
-        } else {
+        }
+        else {
             throw std::logic_error("Hand is empty!!");
         }
     };
@@ -57,13 +65,15 @@ struct House {
     std::string_view name;
     HouseCardHand    cards;
 
-    House(std::string_view n, HouseCardHand&& c) : name{n}, cards{std::move(c)} {}
+    House(std::string_view n, HouseCardHand && c)
+        : name{n}
+        , cards{std::move(c)} {}
 
-    ~House()                = default;
-    House(House&)           = delete;
-    void operator=(House&)  = delete;
-    House(House&&)          = default;
-    void operator=(House&&) = delete;
+    ~House()                 = default;
+    House(House &)           = delete;
+    void operator=(House &)  = delete;
+    House(House &&)          = default;
+    void operator=(House &&) = delete;
 
-    bool operator==(const House& h) const { return name == h.name; }
+    bool operator==(const House & h) const { return name == h.name; }
 };
